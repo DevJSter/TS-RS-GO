288588b9095b7d84e1e8dbcecdbcc30c11361a83
Shubham Tiwari <shubht3303@gmail.com>
2023-11-30 23:35:03 +0530

    ;
    Signed-off-by: Shubham Tiwari <shubht3303@gmail.com>

---
+++ rust/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+ // Integer data types
+ let integer_number: i32 = 42;
+ let unsigned_integer: u64 = 100;
+
+ // Floating-point data types
+ let float_number: f64 = 3.14;
+
+ // Boolean data type
+ let is_rust_cool: bool = true;
+
+ // Character data type
+ let letter: char = 'A';
+
+ // String data type
+ let message: &str = "Hello, Rust!";
+
+ // Displaying values
+ println!("Integer: {}", integer_number);
+ println!("Unsigned Integer: {}", unsigned_integer);
+ println!("Float: {}", float_number);
+ println!("Boolean: {}", is_rust_cool);
+ println!("Character: {}", letter);
+ println!("String: {}", message);
+}
