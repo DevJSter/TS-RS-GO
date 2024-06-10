f7276b1d92933a506b0d8e894837ff5ede8918e2
Shubham Tiwari <shubht3303@gmail.com>
2024-06-11 02:01:22 +0530

    Generica

--- Typescript/practice/index.ts
+++ Typescript/practice/index.ts
@@ -1,10 +1,9 @@
-type Numberarr = (number | string)[]; //arr / string 
-
-const getfirstElement = (arr: Numberarr): (number | string) => {
+// <T> and T[] are known as generics
+function getfirstElement<T> (arr: T[]) {
     return arr[0];
 }
 
-let ans1 =getfirstElement([1,2,3]);
+let ans1 = getfirstElement([1,2,3]);
 let ans2 = getfirstElement(['hello','haribol']);
 
 console.log(ans1);
