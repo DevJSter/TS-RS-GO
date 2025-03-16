0697380bc58112ff7f6a5a30878acccfbc6037f1
Shubham Tiwari <shubht3303@gmail.com>
2025-03-16 23:05:23 +0530

    hm

--- Typescript/SHreyainas/app.ts
+++ Typescript/SHreyainas/app.ts
@@ -47,7 +47,7 @@ meow("SHUBHAM" , (value : string) => {
     
 })
 
-// -? Rest / Spread Parameter
+// - ? Rest / Spread Parameter
 
 
 function sum(...arr : number[]) {
@@ -56,3 +56,68 @@ function sum(...arr : number[]) {
 }
 
 sum(1,2,3,4,5)
+
+
+// ts fnc signatures
+
+function abcd(a : string) : void {
+
+}
+
+// define parms types and return type too
+
+function abcd1( s : string , a : number ) : any {
+    console.log(s);
+    return a; 
+}
+
+// Generics
+
+// Generic Function
+// Generic intgerfaces
+// generix classes
+
+// hum ek fn ko use kart ewakt bata sakte hey ki fn args ko kis type se treat kare
+
+function gege<T>(e: T) {
+
+}
+
+gege<string>("meow") 
+gege<number>(45)
+gege<boolean>(true)
+gege<any>("jdkhvbf")
+gege("helloo") // <string> do ya kais bhii do doesnt matteer
+
+// Generiic Interfaces 
+
+interface meowobj <Hello> {
+    username : string;
+    pass : number;
+    key : Hello
+}
+//  Generics can also be  used | with this 
+function meoowmeow(obj : meowobj<string | number>) {
+
+}
+
+meoowmeow({    
+    username : "shubham",
+    pass : 654,
+    key : 44
+  }
+)
+
+
+class Bottle<B> {
+    constructor(public key : B)
+    {}
+}
+
+let b12 = new Botlle("Cokeak");
+console.log(b12);
+
+// Type assertion 
+// Type casting
+// NOn null assertion operator
+
